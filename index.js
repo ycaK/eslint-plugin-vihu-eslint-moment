72e436a1c99509d10e05a169320c815165d06e04
Kacper Z <djnight538@gmail.com>
2023-03-21 14:24:44 +0000

    package json

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require("path")
 
 const ruleFiles = fs
   .readdirSync(__dirname)
-  .filter((file) => file !== "index.js" && !file.endsWith("test.js"))
+  .filter((file) => file !== "index.js" && !file.endsWith("test.js") && !file.endsWith(".yml"))
 
 const rules = Object.fromEntries(
   ruleFiles.map((file) => [path.basename(file, ".js"), require("./" + file)])
